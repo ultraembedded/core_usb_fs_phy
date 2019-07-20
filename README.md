d62323a54c1b401ce92b4ba21898e4cf800b6648
ultraembedded <ultraembedded.com@gmail.com>
2019-07-20 18:18:36 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
-# core_usb_fs_phy
-USB Full Speed PHY
+### USB Full Speed PHY
+
+Github:   [http://github.com/ultraembedded/core_usb_fs_phy](https://github.com/ultraembedded/core_usb_fs_phy)
+
+This component implements the low level USB1.1 / FS / 12Mbit/s USB signalling (SOP,DATA,EOP) with the required bitstuffing.
+
+##### Features
+* UTMI PHY interface.
+* Connection to a transceiver (e.g. USB1T11A) or can drive the FPGA pins directly as USB D+/D-.
+
+##### Configuration / Requirements
+* Top: usb_fs_phy
+* Clock: clk_i - 48MHz
+* Reset: rst_i - Asynchronous, active high
+
+##### Testing
+Tested with various FPGA + USB based projects.
+
+##### FPGA Transceiver
+To avoid external components, you can drive the USB D+/D- pins directly from the FPGA fabric.
+The USB1.1 bit rate is low enough (12MHz) for this to be not too problematic with short cables!
+
+```
+module usb_fs_phy_wrapper
+(
+     input           clk_i
+    ,input           rst_i
+
+    ,input  [  7:0]  utmi_data_out_i
+    ,input           utmi_txvalid_i
+    ,input  [  1:0]  utmi_op_mode_i
+    ,input  [  1:0]  utmi_xcvrselect_i
+    ,input           utmi_termselect_i
+    ,input           utmi_dppulldown_i
+    ,input           utmi_dmpulldown_i
+    ,output [  7:0]  utmi_data_in_o
+    ,output          utmi_txready_o
+    ,output          utmi_rxvalid_o
+    ,output          utmi_rxactive_o
+    ,output          utmi_rxerror_o
+    ,output [  1:0]  utmi_linestate_o
+
+    // USB D+ / D-
+    ,inout          usb_dp_io
+    ,inout          usb_dn_io
+);
+
+wire           usb_pads_rx_rcv_w;
+wire           usb_pads_rx_dn_w;
+wire           usb_pads_rx_dp_w;
+wire           usb_pads_tx_dn_w;
+wire           usb_pads_tx_dp_w;
+wire           usb_pads_tx_oen_w;
+
+usb_transceiver u_usb_xcvr
+(
+    // Inputs
+     .usb_phy_tx_dp_i(usb_pads_tx_dp_w)
+    ,.usb_phy_tx_dn_i(usb_pads_tx_dn_w)
+    ,.usb_phy_tx_oen_i(usb_pads_tx_oen_w)
+    ,.mode_i(1'b1)
+
+    // Outputs
+    ,.usb_dp_io(usb_dp_io)
+    ,.usb_dn_io(usb_dn_io)
+    ,.usb_phy_rx_rcv_o(usb_pads_rx_rcv_w)
+    ,.usb_phy_rx_dp_o(usb_pads_rx_dp_w)
+    ,.usb_phy_rx_dn_o(usb_pads_rx_dn_w)
+);
+
+usb_fs_phy u_usb_phy
+(
+    // Inputs
+     .clk_i(clk_i)
+    ,.rst_i(rst_i)
+    ,.utmi_data_out_i(utmi_data_out_i)
+    ,.utmi_txvalid_i(utmi_txvalid_i)
+    ,.utmi_op_mode_i(utmi_op_mode_i)
+    ,.utmi_xcvrselect_i(utmi_xcvrselect_i)
+    ,.utmi_termselect_i(utmi_termselect_i)
+    ,.utmi_dppulldown_i(utmi_dppulldown_i)
+    ,.utmi_dmpulldown_i(utmi_dmpulldown_i)
+    ,.usb_rx_rcv_i(usb_pads_rx_rcv_w)
+    ,.usb_rx_dp_i(usb_pads_rx_dp_w)
+    ,.usb_rx_dn_i(usb_pads_rx_dn_w)
+    ,.usb_reset_assert_i(1'b0)
+
+    // Outputs
+    ,.utmi_data_in_o(utmi_data_in_o)
+    ,.utmi_txready_o(utmi_txready_o)
+    ,.utmi_rxvalid_o(utmi_rxvalid_o)
+    ,.utmi_rxactive_o(utmi_rxactive_o)
+    ,.utmi_rxerror_o(utmi_rxerror_o)
+    ,.utmi_linestate_o(utmi_linestate_o)
+    ,.usb_tx_dp_o(usb_pads_tx_dp_w)
+    ,.usb_tx_dn_o(usb_pads_tx_dn_w)
+    ,.usb_tx_oen_o(usb_pads_tx_oen_w)
+    ,.usb_reset_detect_o()
+    ,.usb_en_o()
+);
+
+endmodule
+```
+
+Example contraints;
+```
+set_property -dict { PACKAGE_PIN N4    IOSTANDARD LVCMOS33 } [get_ports { usb_dp_io }];
+set_property -dict { PACKAGE_PIN P3    IOSTANDARD LVCMOS33 } [get_ports { usb_dn_io }];
+set_property PULLUP TRUE [get_ports usb_dp_io]
+```
+
+##### References
+* [UTMI Specification](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/usb2-transceiver-macrocell-interface-specification.pdf)
+* [USB1T11A](http://www.mouser.com/ds/2/149/fairchild%20semiconductor_usb1t11a-320893.pdf)
---
+++ src_v/usb_transceiver.v
@@ -0,0 +1,162 @@
+//-----------------------------------------------------------------
+//                     USB Full Speed (12mbps) Phy
+//                              V0.1
+//                        Ultra-Embedded.com
+//                          Copyright 2015
+//
+//                 Email: admin@ultra-embedded.com
+//
+//                         License: LGPL
+//-----------------------------------------------------------------
+//
+// This source file may be used and distributed without         
+// restriction provided that this copyright statement is not    
+// removed from the file and that any derivative work contains  
+// the original copyright notice and the associated disclaimer. 
+//
+// This source file is free software; you can redistribute it   
+// and/or modify it under the terms of the GNU Lesser General   
+// Public License as published by the Free Software Foundation; 
+// either version 2.1 of the License, or (at your option) any   
+// later version.
+//
+// This source is distributed in the hope that it will be       
+// useful, but WITHOUT ANY WARRANTY; without even the implied   
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      
+// PURPOSE.  See the GNU Lesser General Public License for more 
+// details.
+//
+// You should have received a copy of the GNU Lesser General    
+// Public License along with this source; if not, write to the 
+// Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+// Boston, MA  02111-1307  USA
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+//                          Generated File
+//-----------------------------------------------------------------
+
+module usb_transceiver
+(
+    // Inputs
+     input           usb_phy_tx_dp_i
+    ,input           usb_phy_tx_dn_i
+    ,input           usb_phy_tx_oen_i
+    ,input           mode_i
+
+    // Outputs
+    ,inout          usb_dp_io
+    ,inout          usb_dn_io
+    ,output          usb_phy_rx_rcv_o
+    ,output          usb_phy_rx_dp_o
+    ,output          usb_phy_rx_dn_o
+);
+
+
+
+//-----------------------------------------------------------------
+// Module: usb_transceiver
+// Emulate standard USB PHY interface and produce a D+/D- outputs.
+// Allows direct connection of USB port to FPGA.
+// Limitations:
+// As no differential amplifier present, no common mode noise
+// rejection occurs.
+// Unlikely to work well with longer connections!
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// Wires
+//-----------------------------------------------------------------
+reg out_dp;
+reg out_dn;
+
+//-----------------------------------------------------------------
+// Assignments
+//-----------------------------------------------------------------
+
+// D+/D- Tristate buffers
+assign usb_dp_io = (usb_phy_tx_oen_i == 1'b0) ? out_dp : 1'bz;
+assign usb_dn_io = (usb_phy_tx_oen_i == 1'b0) ? out_dn : 1'bz;
+
+// Receive D+/D-
+assign usb_phy_rx_dp_o = usb_dp_io;
+assign usb_phy_rx_dn_o = usb_dn_io;
+
+// Receive output
+assign usb_phy_rx_rcv_o = (usb_dp_io == 1'b1 && usb_dn_io == 1'b0) ? 1'b1 : 1'b0;
+
+// PHY Transmit Mode:
+// When phy_tx_mode_i is '0' the outputs are encoded as:
+//     vmo_i, vpo_i
+//      0    0    Differential Logic '0'
+//      0    1    Differential Logic '1'
+//      1    0    Single Ended '0'
+//      1    1    Single Ended '0'
+// When phy_tx_mode_i is '1' the outputs are encoded as:
+//     vmo_i, vpo_i
+//      0    0    Single Ended '0'
+//      0    1    Differential Logic '1'
+//      1    0    Differential Logic '0'
+//      1    1    Illegal State
+always @ (mode_i or usb_phy_tx_dp_i or usb_phy_tx_dn_i)
+begin : MUX
+ case(mode_i)
+    1'b0:
+    begin
+        if (usb_phy_tx_dp_i == 1'b0 && usb_phy_tx_dn_i == 1'b0)
+        begin
+            // Logic "0"
+            out_dp = 1'b0;
+            out_dn = 1'b1;
+        end
+        else if (usb_phy_tx_dp_i == 1'b0 && usb_phy_tx_dn_i == 1'b1)
+        begin
+            // SE0 (both low)
+            out_dp = 1'b0;
+            out_dn = 1'b0;
+        end
+        else if (usb_phy_tx_dp_i == 1'b1 && usb_phy_tx_dn_i == 1'b0)
+        begin
+            // Logic "1"
+            out_dp = 1'b1;
+            out_dn = 1'b0;
+        end
+        else if (usb_phy_tx_dp_i == 1'b1 && usb_phy_tx_dn_i == 1'b1)
+        begin
+            // SE0 (both low)
+            out_dp = 1'b0;
+            out_dn = 1'b0;
+        end
+    end
+    1'b1 :
+    begin
+        if (usb_phy_tx_dp_i == 1'b0 && usb_phy_tx_dn_i == 1'b0)
+        begin
+            // SE0 (both low)
+            out_dp = 1'b0;
+            out_dn = 1'b0;
+        end
+        else if (usb_phy_tx_dp_i == 1'b0 && usb_phy_tx_dn_i == 1'b1)
+        begin
+            // Logic "0"
+            out_dp = 1'b0;
+            out_dn = 1'b1;
+        end
+        else if (usb_phy_tx_dp_i == 1'b1 && usb_phy_tx_dn_i == 1'b0)
+        begin
+            // Logic "1"
+            out_dp = 1'b1;
+            out_dn = 1'b0;
+        end
+        else if (usb_phy_tx_dp_i == 1'b1 && usb_phy_tx_dn_i == 1'b1)
+        begin
+            // Illegal
+            out_dp = 1'b1;
+            out_dn = 1'b1;
+        end
+    end
+ endcase
+end
+
+
+endmodule
